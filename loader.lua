4115b591c2e24d86f47b32e26bf4e798a344cbce
iRay888 <80120542+iRay888@users.noreply.github.com>
2024-12-12 17:48:35 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,10 +1,21 @@
 -- https://discord.gg/tUEJZYvF9d
--- known to work on wave, nihon, awp.gg, syn z, seliware
-local environment = identifyexecutor and identifyexecutor()
+-- known to work on wave, nihon, awp.gg, syn z, seliware, sirhurt
+local environment = identifyexecutor and identifyexecutor() or ""
 local source = game:HttpGet("https://raw.githubusercontent.com/iRay888/wapus/refs/heads/main/source.lua")
 
 if getfflag and string.find(string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))), "true") and not executed then
     loadstring(source)()
+elseif string.find(environment, "AWP") ~= nil and not executed then
+    for _, v in getactors() do
+        run_on_actor(v, [[
+            for _, func in getgc(false) do
+                if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
+                    ]] .. source .. [[
+                    break
+                end
+            end
+        ]])
+    end
 elseif environment == "Wave" and not executed then
     run_on_actor(getdeletedactors()[1], source)
 elseif environment == "Nihon" and not executed then
